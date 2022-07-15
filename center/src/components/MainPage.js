58b03167eef37339a4bf61c805dec48e73960cae
Good Do <56465252+genegods@users.noreply.github.com>
2022-07-15 18:19:07 +0800

    Add files via upload

---
+++ center/src/components/MainPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <React.Fragment>
+    <section>
+      <div className="centered w-full h-screen">
+        <div className=" bg-yellow-500 w-96 h-96 centered">
+            <div className="bg-green-500 w-60 h-60 centered ">
+                <div className="bg-blue-500 w-32 h-32 centered">
+                    <div className="bg-gray-700 w-12 h-12"></div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  </React.Fragment>
+  )
+}
+
+export default MainPage
